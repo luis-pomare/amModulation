25e41c39442d9cdeacf4ad0e1bb8312763a75231
Luis Pomare <luispomare92@gmail.com>
2022-11-30 07:10:08 -0500

    Implement the draw point method

--- src/modulacionam/plano.java
+++ src/modulacionam/plano.java
@@ -7,6 +7,8 @@ public class plano {
     private JPanel p;
     private int w;
     private int h;
+    private int escala = 10;
+    
     
     public plano(JPanel p) {
         this.p = p;
@@ -15,26 +17,34 @@ public class plano {
     }
     
     public void dibujar(){
-        Graphics2D g= (Graphics2D) p.getGraphics();        
+        Graphics2D g= (Graphics2D) p.getGraphics();
         
         //lineas verticales
-        for(int x=w/2; x<w;x+=10){
+        for(int x=w/2; x<w;x+=escala){
             g.drawLine(x , 0, x,h );            
         }
-        for(int x=w/2; x>0;x-=10){
+        for(int x=w/2; x>0;x-=escala){
             g.drawLine(x , 0, x,h );            
         }
         
         //lineas horizontales
-        for(int y=h/2; y<h;y+=10){
+        for(int y=h/2; y<h;y+=escala){
             g.drawLine(0 , y, w,y );            
         }
-        for(int y=h/2; y>0;y-=10){
+        for(int y=h/2; y>0;y-=escala){
             g.drawLine(0 , y, w,y );            
         }
         
         //Lineas del medio
+        g.setColor(Color.red);
         g.drawLine(w/2, 0, w/2,h );
         g.drawLine(0, h/2, w,h/2 );
+    }
+    
+    public void dibujarPunto(int x, int y){
+        Graphics2D g= (Graphics2D) p.getGraphics();
+        int realX = w/2+x*escala;
+        int realY = h/2-y*escala;
+        g.fillOval(realX-3, realY-3, 6, 6);
     }
 }
