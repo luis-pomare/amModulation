a8c39480016f8ea6084dc605b6cea8f834ab5e75
Luis Pomare <luispomare92@gmail.com>
2022-12-12 03:39:02 -0500

    Add library

--- src/modulacionam/ModulacionAM.java
+++ src/modulacionam/ModulacionAM.java
@@ -1,4 +1,12 @@
 package modulacionam;
+import com.slabs.SLabsTimer;
+import com.slabs.TriggerListener;
+import com.slabs.modulation.analog.AM;
+import com.slabs.sinks.FFTScope;
+import com.slabs.sinks.Scope;
+import com.slabs.sources.PulseTrain;
+import com.slabs.sources.SawTooth;
+import com.slabs.sources.SineWave;
 public class ModulacionAM {
     public static void main(String[] args) {
         ventanaPrincipal modulador = new ventanaPrincipal();
